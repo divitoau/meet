b76621c48cb586a6afd63e056370f292cfb1e8a5
divitoau <divitoau@gmail.com>
2023-08-13 19:28:05 -0400

    integrated number of events textbox functionality with the display of events

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,13 +1,16 @@
-import { render } from "@testing-library/react";
+import { render, waitFor, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import NumberOfEvents from "../components/NumberOfEvents";
+import App from "../App";
 
 describe("<NumberOfEvents /> component", () => {
   let NumberOfEventsComponent;
   let NumberOfEventsTextbox;
   beforeEach(() => {
-    NumberOfEventsComponent = render(<NumberOfEvents />);
+    NumberOfEventsComponent = render(
+      <NumberOfEvents currentNOE={32} setCurrentNOE={() => {}} />
+    );
     NumberOfEventsTextbox = NumberOfEventsComponent.queryByRole("textbox");
   });
 
@@ -23,5 +26,20 @@ describe("<NumberOfEvents /> component", () => {
     const user = userEvent.setup();
     await user.type(NumberOfEventsTextbox, "{backspace}{backspace}10");
     expect(NumberOfEventsTextbox).toHaveValue("10");
+  });
+});
+
+describe("<NumberOfEvents /> integration", () => {
+  test("changes number of events displayed when a new number is entered", async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const NumberOfEventsDOM = AppDOM.querySelector("#number-of-events");
+    const NumberOfEventsTextbox =
+      within(NumberOfEventsDOM).queryByRole("textbox");
+    const EventListDOM = AppDOM.querySelector("#event-list");
+    await user.type(NumberOfEventsTextbox, "{backspace}{backspace}10");
+    const EventListItems = within(EventListDOM).queryAllByRole("listitem");
+    expect(EventListItems.length).toBe(parseInt(NumberOfEventsTextbox.value));
   });
 });

c36cdc54d71bda57220a40ae77eacb142e12dd12
divitoau <divitoau@gmail.com>
2023-08-15 01:29:52 -0400

    created end to end tests for filtering events by city

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,16 +1,51 @@
 import puppeteer from "puppeteer";
 
+describe("filter events by city", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch(/* {
+      headless: false,
+      slowMo: 250,
+      timeout: 0,
+    } */);
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  let events;
+  test("When user hasn't searched for a specific city, show upcoming events from all cities", async () => {
+    events = await page.$$(".event");
+    expect(events).toHaveLength(32);
+  });
+
+  test("User should see a list of suggestions when they search for a city", async () => {
+    await page.type(".city", "Berlin");
+    const suggestionListItems = await page.$$(".suggestions li");
+    expect(suggestionListItems).toHaveLength(2);
+  });
+
+  test("User can select a city from the suggested list", async () => {
+    await page.click(".suggestions li");
+    events = await page.$$(".event");
+    expect(events).toHaveLength(17);
+  });
+});
+
 describe("show/hide an event details", () => {
   let browser;
   let page;
   beforeAll(async () => {
-    browser = await puppeteer.launch(
-/*       {
+    browser = await puppeteer.launch(/* {
       headless: false,
       slowMo: 250,
       timeout: 0,
-      } */
-    );
+    } */);
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event");

729010ce035582a0624323435f7ea0d1623fb7b4
divitoau <divitoau@gmail.com>
2023-08-16 19:41:45 -0400

    incorporated service worker

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import * as atatus from "atatus-spa";
 atatus.config("ec4d9a8b69b34e2c81fb8e4c2462ad8a").install();
 
@@ -12,6 +13,11 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
